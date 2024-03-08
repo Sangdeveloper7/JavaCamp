ea873ef2c004041e15eb95b23d4020f68d9d2537
SangHyunLee <22000516@handong.ac.kr>
2024-03-08 15:23:14 +0900

    third commit

--- Desktop/3-2/C001.c
+++ Desktop/3-2/C001.c
@@ -1,13 +1,46 @@
 #include <stdio.h>
 
 int main(){
-    int size, stars, blank1, blank2 = 0;
+    int size, stars, blank1, blank2;
 
     scanf("%d",&size);
-    stars = 2*size;
-    for(int i =0; i< size; i++){
-        printf("**");
-    }
 
-    return 0;
+    for(int i=0; i<size; i++){
+        for(int j=0; j<i; j++){
+            printf(" ");
+        }
+        printf("*");
+        blank1 = 2*(size - i -1);
+        for(int j=0; j<blank1; j++){
+            printf(" ");
+        }
+        printf("*\n");
+    }  /// 첫번째 트로피 모양 
+
+    for(int i=0; i<size; i++){
+        for(int j=0; j<2*size; j++){
+            printf("*");
+        }
+        printf("\n");
+    } // 중간 5개 원통형 
+
+    for(int i=0; i<size; i++){
+        for(int j=0; j<i; j++){
+            printf(" ");
+        }
+        for(int j=0; j<2*(size-i); j++){
+            printf("*");
+        }
+        for(int j=0; j<i; j++){
+            printf(" ");
+        }
+        printf("\n");
+    } // 밑으로 갈수록 작아지는 피라미드 
+
+    for(int i=0; i<2*size; i++){
+        printf("*");
+    }
+    
+
+    return 0; 
 }
---
+++ Desktop/3-2/C002.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(){
+    int person[4][3];
+    int sum[3] = {0, 0, 0};
+    float ratio[3];
+    int i, j;
+    printf("Check room (Men) \n");
+    for(i = 0; i< 2; i++){
+        for(int j=0; j<3; j++){
+            printf("Room #%d0%d >  ", i+1, j+1);
+            scanf("%d", &person[i][j]);
+        }
+    } // 남자방 인원수 입력 
+
+    printf("Check room (Women) \n");
+    for(i = 2; i< 4; i++){
+        for(int j=0; j<3; j++){
+            printf("Room #%d0%d >  ", i+1, j+1);
+            scanf("%d", &person[i][j]);
+        }
+    } // 여자방 인원수 입력 
+
+    for(int i=0; i<2; i++){
+        for(int j=0; j<3; j++){
+            sum[0] += person[i][j];
+        }
+    } // 남자방 인원수 합
+
+    printf("%d", sum[0]);
+
+    for(int i=2; i<4; i++){
+        for(int j=0; j<3; j++){
+            sum[1] += person[i][j];
+        }
+    } // 여자방 인원수 합 
+
+    sum[2] = sum[0] + sum[1];
+    ratio[0] = sum[0] / 24.0;
+    ratio[1] = sum[1] / 24.0;
+    ratio[2] = sum[2] / 48.0;
+
+    printf("Men: %d/24 (%.1f%%)\n", sum[0], ratio[0]*100);
+    printf("Women: %d/24 (%.1f%%)\n", sum[1], ratio[1]*100);    
+    printf("Total: %d/48 (%.1f%%)\n", sum[2], ratio[2]*100);
+
+
+    return 0;
+}
